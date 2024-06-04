5dbb55e25f067a6e7a0047fb5e4c212349bf4393
Antoš Michael <antosmichael07@gmail.com>
2024-06-04 21:04:59 +0200

    added a func LogLevel

--- logger.go
+++ logger.go
@@ -10,7 +10,6 @@ import (
 type Logger struct {
 	Directory string
 	Output    Output
-	Level     int
 }
 
 type Output struct {
@@ -29,7 +28,34 @@ func NewLogger() Logger {
 }
 
 func (logger Logger) Log(message string, args ...interface{}) {
-	_, caller, line, _ := runtime.Caller(logger.Level)
+	_, caller, line, _ := runtime.Caller(1)
+	for i := len(caller) - 1; i >= 0; i-- {
+		if caller[i] == '/' {
+			caller = caller[i+1:]
+			break
+		}
+	}
+
+	msg := fmt.Sprintf(message, args...)
+	str := fmt.Sprintf("[%s] [%s:%d] %s\n", time.Now().String()[:19], caller, line, msg)
+
+	if logger.Output.Console {
+		fmt.Print(str)
+	}
+
+	if logger.Output.File {
+		if _, err := os.Stat(fmt.Sprintf("./%s", logger.Directory)); os.IsNotExist(err) {
+			os.Mkdir(fmt.Sprintf("./%s", logger.Directory), 0755)
+		}
+
+		file, _ := os.OpenFile(fmt.Sprintf("./%s/%s.txt", logger.Directory, time.Now().String()[:10]), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		file.WriteString(str)
+		file.Close()
+	}
+}
+
+func (logger Logger) LogLevel(level int, message string, args ...interface{}) {
+	_, caller, line, _ := runtime.Caller(level)
 	for i := len(caller) - 1; i >= 0; i-- {
 		if caller[i] == '/' {
 			caller = caller[i+1:]
